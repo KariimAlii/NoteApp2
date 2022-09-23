856f278f1011a6c360f40be1c8c4dbc756b63fcf
Karim Ali <kariimalii96@gmail.com>
2022-09-23 23:09:32 +0200

    using command line arguments

--- app.js
+++ app.js
@@ -10,17 +10,38 @@ const getNotes = require("./notes.js");
 const validator = require("validator");
 const chalk = require("chalk");
 //============== Using my Own Module ===============//
-console.log(getNotes("I live here!"));
+// console.log(getNotes("I live here!"));
 //============== using Validator module ===============//
-console.log(validator.isEmail("kariimkiko%gmail.com"));
-console.log(validator.isEmail("kariimkiko@gmail.com"));
-console.log(validator.isURL("krkr.io"));
-console.log(validator.isURL("https://krkr.io"));
-console.log(validator.isURL("https:/krkr.io"));
+// console.log(validator.isEmail("kariimkiko%gmail.com"));
+// console.log(validator.isEmail("kariimkiko@gmail.com"));
+// console.log(validator.isURL("krkr.io"));
+// console.log(validator.isURL("https://krkr.io"));
+// console.log(validator.isURL("https:/krkr.io"));
 //============== Using Chalk Module ===============//
-const failure = chalk.bold.red.inverse("faaaail!!!!");
-console.log(chalk.blue.bgYellowBright("Okkaaaa"));
-console.log(chalk.green.inverse("succeed"));
-console.log(failure);
+// const failure = chalk.bold.red.inverse("faaaail!!!!");
+// console.log(chalk.blue.bgYellowBright("Okkaaaa"));
+// console.log(chalk.green.inverse("succeed"));
+// console.log(failure);
 //============== Using Nodemon Module globally ===============//
-console.log(chalk.bold.green.inverse("here is nodemon"));
+// console.log(chalk.bold.green.inverse("here is nodemon"));
+//============== command line arguments process.argv ===============//
+// console.log(process.argv[2]);
+const command = process.argv[2];
+command === "add"
+    ? console.log(chalk.green.inverse("Adding Note!"))
+    : command === "remove"
+    ? console.log(chalk.red.inverse("Removing Note!"))
+    : console.log(chalk.white.inverse("Enter a command!"));
+/*
+const result = process.argv[3];
+result >= 0
+    ? (result == 0 ? console.log('Zero!!') : console.log('Positive!!') )
+    : console.log('Negative!') 
+//------- OR --------//
+const result = 
+process.argv[3] >= 0
+    ? (process.argv[3] == 0 ? 'Zero' : 'Positive')
+    :'Negative'
+console.log(result)
+*/
+console.log(process.argv)
