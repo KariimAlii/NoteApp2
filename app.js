9407775bd19aad00ea8bae828929abab450f8afc
Karim Ali <kariimalii96@gmail.com>
2022-09-23 22:32:38 +0200

    added npm packages

--- app.js
+++ app.js
@@ -5,5 +5,22 @@
 // const sum = require("./utils.js");
 // console.log(sum(2, 3));
 
+//============== Modules ===============//
 const getNotes = require("./notes.js");
-console.log(getNotes('Ana gamed'))
+const validator = require("validator");
+const chalk = require("chalk");
+//============== Using my Own Module ===============//
+console.log(getNotes("I live here!"));
+//============== using Validator module ===============//
+console.log(validator.isEmail("kariimkiko%gmail.com"));
+console.log(validator.isEmail("kariimkiko@gmail.com"));
+console.log(validator.isURL("krkr.io"));
+console.log(validator.isURL("https://krkr.io"));
+console.log(validator.isURL("https:/krkr.io"));
+//============== Using Chalk Module ===============//
+const failure = chalk.bold.red.inverse("faaaail!!!!");
+console.log(chalk.blue.bgYellowBright("Okkaaaa"));
+console.log(chalk.green.inverse("succeed"));
+console.log(failure);
+//============== Using Nodemon Module globally ===============//
+console.log(chalk.bold.green.inverse("here is nodemon"));
