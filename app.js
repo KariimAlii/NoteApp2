3f5bf3c78b590b3b107cdae8e3161121e6ae4e06
Karim Ali <kariimalii96@gmail.com>
2022-09-26 16:37:06 +0200

    starting work with yargs

--- app.js
+++ app.js
@@ -9,6 +9,10 @@
 const getNotes = require("./notes.js");
 const validator = require("validator");
 const chalk = require("chalk");
+
+const { hideBin } = require("yargs/helpers");
+const yargs = require("yargs/yargs")(hideBin(process.argv)); // === (const yargs = require("yargs/yargs")(process.argv.slice(2));)
+const argv = yargs.argv;
 //============== Using my Own Module ===============//
 // console.log(getNotes("I live here!"));
 //============== using Validator module ===============//
@@ -26,12 +30,14 @@ const chalk = require("chalk");
 // console.log(chalk.bold.green.inverse("here is nodemon"));
 //============== command line arguments process.argv ===============//
 // console.log(process.argv[2]);
+/*
 const command = process.argv[2];
 command === "add"
     ? console.log(chalk.green.inverse("Adding Note!"))
     : command === "remove"
     ? console.log(chalk.red.inverse("Removing Note!"))
     : console.log(chalk.white.inverse("Enter a command!"));
+*/
 /*
 const result = process.argv[3];
 result >= 0
@@ -44,4 +50,30 @@ process.argv[3] >= 0
     :'Negative'
 console.log(result)
 */
-console.log(process.argv)
+//console.log(process.argv);
+//console.log(argv);
+
+// Customize yargs version
+// yargs.version('1.1.0')
+
+// Create add command
+
+yargs.command("add", "Add a new note", () => {
+    console.log(chalk.bold.green.inverse("Adding a new Noteee!!!"));
+}).argv;
+
+// Create remove command
+yargs.command("remove", "Remove a note", () => {
+    console.log(chalk.bold.red.inverse("Removing a note!!"));
+}).argv;
+
+// Create read command
+yargs.command("read", "Read a note", () => {
+    console.log(chalk.bold.magenta.inverse("Reading a note"), argv.title);
+}).argv;
+
+// Create a list command
+yargs.command("list", "List the notes", () => {
+    console.log(chalk.bold.yellow.inverse("Listing your Notes!!"));
+}).argv;
+console.log(__dirname)
